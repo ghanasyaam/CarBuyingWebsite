e3353a561508a9318a2ff643b7986c6d8100b249
ghanasyam s sunil <ghanasyaaam@gmail.com>
2024-05-27 20:14:27 +0530

    idk

--- src/Root.js
+++ src/Root.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Quote from './Pages/Homepage/Quote';
-import AdminPage from './Pages/AdminPage/AdminPage';
 import Home from './Pages/UserAuthentication/Home';
 import Login from './Pages/UserAuthentication/Login';
 import Signup from './Pages/UserAuthentication/Signup';
@@ -13,7 +12,6 @@ function Root() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/quote" element={<Quote />} />
-        <Route path="/admin" element={<AdminPage />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/home" element={<Home/>}/>
