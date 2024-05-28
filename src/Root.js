95ffcfb7bf826ec2b67463579d88f90b9cca1aaf
ghanasyam s sunil <ghanasyaaam@gmail.com>
2024-05-28 09:32:18 +0530

    Tweaks

--- src/Root.js
+++ src/Root.js
@@ -5,6 +5,7 @@ import Quote from './Pages/Homepage/Quote';
 import Home from './Pages/UserAuthentication/Home';
 import Login from './Pages/UserAuthentication/Login';
 import Signup from './Pages/UserAuthentication/Signup';
+import Admin from './Pages/Admin/Admin';
 
 function Root() {
   return (
@@ -15,6 +16,7 @@ function Root() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/admin" element={<Admin/>}/>
       </Routes>
     </BrowserRouter>
   );
